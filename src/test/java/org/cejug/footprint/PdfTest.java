46bd7049d712724de0ded7861945eba09bd62897
eprogramming <00hf11@gmail.com>
2014-04-06 13:59:46 -0300

    This closes #30

--- src/test/java/org/cejug/footprint/PdfTest.java
+++ src/test/java/org/cejug/footprint/PdfTest.java
@@ -1,6 +1,7 @@
 package org.cejug.footprint;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.Set;
 
@@ -12,38 +13,33 @@ import com.itextpdf.text.pdf.PdfStamper;
 
 public class PdfTest {
 
-    private static final String SAMPLE = "samples.pdf";
-    
+    private static final String SAMPLE = "example.pdf";
+
     @Test
     public void should_print_the_values_of_pdf_sample() throws Exception {
-        
+
         URL url = this.getClass().getResource(SAMPLE);
         PdfReader reader = new PdfReader(url);
         AcroFields fields = reader.getAcroFields();
         Set<String> keys = fields.getFields().keySet();
         for (String k : keys) {
-          System.out.println(k + ": " + fields.getField(k));
+            System.out.println(k + ": " + fields.getField(k));
         }
-        
+
     }
-    
+
     @Test
     public void should_write_the_values_of_pdf_sample() throws Exception {
-        
+
         URL url = this.getClass().getResource(SAMPLE);
         PdfReader reader = new PdfReader(url);
-        AcroFields fields = reader.getAcroFields();
-        Set<String> keys = fields.getFields().keySet();
-        
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PdfStamper stamper = new PdfStamper(reader, baos);
-        
-        for (String k : keys) {
-          fields.setField(fields.getField(k),fields.getField(k));
-        }
-        
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream("result.pdf"));
+        AcroFields form = stamper.getAcroFields();
+        form.setField("memberName", "12345");
+        form.setField("groupName", "6789");
+        form.setField("eventName", "101112");
         stamper.close();
-        
+
     }
-    
+
 }
